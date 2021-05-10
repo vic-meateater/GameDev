8a5c5d6d6ea60bb7ed9355f545b536d0ed749c8e
vic meateater <meateater.jobs@gmail.com>
2021-05-10 16:03:41 +0500

    Домашняя работа 4 (1,2,3)

---
+++ HomeWork4/MyLibrary/Infrastructure/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EX_1.Presenter;
+
+namespace EX_1.Infrastructure
+{
+    public class Menu
+    {
+        public static void LetsRoll()
+        {
+            Massivius massivius = new Massivius(new View.ConsoleView());
+            massivius.Devider();
+            massivius.DeviderFromFile();
+            massivius.MyArrayPrint();
+            Console.ReadLine();
+            
+        }
+    }
+}
